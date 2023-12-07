48ed9a96efd3542a450cf8fee7b316167c87ebc9
Alvin <xdalvin11@gmail.com>
2023-12-07 10:19:29 +0800

    getters & setters

--- 2023_12_07/main.py
+++ 2023_12_07/main.py
@@ -1,5 +1,31 @@
-def main():
-    exit(print("0"))
+# using getters & setters
+class Student:
+
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def student_name(self):
+        print(f'"{self.name}" was accessed.')
+        return self.name
+    
+    @student_name.setter
+    def student_name(self, values):
+        print(f'"{self.name}" is now {values} .')
+        self.name = values
+    
+    @student_name.deleter
+    def student_name(self):
+        print(f'"{self.name}" was delete.')
+        del self.name
+
 
 if __name__ == '__main__':
-    main()
+    st1 = Student("xxxxx")
+
+    print(st1.student_name)
+    st1.student_name = 'ooooo'
+    del st1.student_name
+
+    print(st1.student_name)
+    
